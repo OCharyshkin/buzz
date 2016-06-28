0a775887fd862526d35c1c0204feec158f89e565
ocharyshkin <o.charyshkin@gmail.com>
2016-06-28 09:22:15 +0300

    * added env variables reading

--- index.js
+++ index.js
@@ -5,6 +5,19 @@ var _ = require('underscore');
 
 var app = express();
 
+var databaseUri = process.env.DATABASE_URI;
+
+var port = process.env.PORT;
+var serverUrl = process.env.SERVER_URL;
+
+//var api = new ParseServer({
+//  databaseURI: databaseUri,
+//  cloud: process.env.CLOUD_CODE_MAIN || __dirname + '/cloud/main.js',
+//  appId: process.env.APP_ID,
+//  masterKey: process.env.MASTER_KEY, //Add your master key here. Keep it secret!
+//  serverURL: serverUrl + ':' + port  // Don't forget to change to https if needed
+//});
+
 app.get('/', function(request, response) {
   response.send('Hello World!');
 });
