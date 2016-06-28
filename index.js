e4fd526e4310e7e6fec93027f271a70a480c6e8a
ocharyshkin <o.charyshkin@gmail.com>
2016-06-28 09:29:15 +0300

    * added log

--- index.js
+++ index.js
@@ -6,23 +6,27 @@ var _ = require('underscore');
 var app = express();
 
 var databaseUri = process.env.DATABASE_URI;
-
 var port = process.env.PORT;
 var serverUrl = process.env.SERVER_URL;
 
-var api = new ParseServer({
-  databaseURI: databaseUri,
-  cloud: process.env.CLOUD_CODE_MAIN || __dirname + '/cloud/main.js',
-  appId: process.env.APP_ID,
-  masterKey: process.env.MASTER_KEY, //Add your master key here. Keep it secret!
-  serverURL: serverUrl + ':' + port  // Don't forget to change to https if needed
-});
+console.log("variables")
+console.log(databaseUri)
+console.log(serverUrl)
+console.log(process.env.APP_ID)
+console.log(process.env.MASTER_KEY)
+
+//var api = new ParseServer({
+//  databaseURI: databaseUri,
+//  cloud: process.env.CLOUD_CODE_MAIN || __dirname + '/cloud/main.js',
+//  appId: process.env.APP_ID,
+//  masterKey: process.env.MASTER_KEY, //Add your master key here. Keep it secret!
+//  serverURL: serverUrl + ':' + port  // Don't forget to change to https if needed
+//});
 
 app.get('/', function(request, response) {
   response.send('Hello World!');
 });
 
-var port = process.env.PORT || 5000;
 app.listen(port, function() {
   console.log("Listening on " + port);
 });
