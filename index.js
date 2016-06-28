3291aeebec3f2ea725242952b302fe84f36a9d66
ocharyshkin <o.charyshkin@gmail.com>
2016-06-28 10:45:20 +0300

    * added fb app id to login

--- index.js
+++ index.js
@@ -20,7 +20,7 @@ var api = new ParseServer({
   clientKey: process.env.CLIENT_KEY,
   masterKey: process.env.MASTER_KEY, //Add your master key here. Keep it secret!
   serverURL: serverUrl,  // Don't forget to change to https if needed
-  facebookAppIds: [874455285995225]
+  facebookAppIds: [process.env.FB_APP_ID]
 });
 // Client-keys like the javascript key or the .NET key are not necessary with parse-server
 // If you wish you require them, you can set them as options in the initialization above:
